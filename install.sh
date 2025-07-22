b110a8ad12ca2bbe58d46f7a5a619d09bee3845c
aezakku <isaacrevenge@gmail.com>
2025-07-22 22:00:34 +0700

    add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+cp -r tmux/.tmux.conf ~/ 
+cp -r tmux/.tmux ~/
+cp -r nvim ~/.config/
+cp -r i3/i3 ~/.config
+cp alacritty/ ~/.config
+cp gdb/.gdbinit ~/
+cp -r gdb/gdb ~/.config
+cp -r polybar/polybar ~/.config
+cp -r rofi/rofi ~/.config
+cp -r zsh/.zshrc ~/
